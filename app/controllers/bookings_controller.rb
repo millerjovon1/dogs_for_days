e6733251685d1837005a1812dae0e4445e5c9938
Devi Putri <devi.putri@live.com>
2023-08-15 14:33:12 +0900

    added the booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = @current_user.booking
+  end
+
+  def new
+@dog = Dog.find(params[:dog_id])
+@booking = Booking.new
+ end
+
+ def create
+  @dog = Dog.find(params[:dog_id])
+  @booking = Booking.new(booking_params)
+  @booking.dog = @dog
+  if @booking.save
+    redirect_to dog_path(@dog)
+  else
+    render :new, status: :unprocessable_entity
+  end
+ end
+
 end
