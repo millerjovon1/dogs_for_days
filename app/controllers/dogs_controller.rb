3b06ee5a58285ff745f206a3dbb1d7103ca5f36b
Lili Banks <lili88banks@icloud.com>
2023-08-16 12:56:04 +0900

    rghuowrgj

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -15,7 +15,7 @@ class DogsController < ApplicationController
   def create
     @dog = Dog.new(dog_params)
     @dog.user = current_user
-    
+
     if @dog.save
       redirect_to dog_path(@dog)
     else
