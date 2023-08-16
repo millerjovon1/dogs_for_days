211954a14c6d926a0ca90768dc6fdf4f4a6f2311
Jovon Miller <130570205+millerjovon1@users.noreply.github.com>
2023-08-16 14:47:28 +0900

    heroku test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @future_bookings = current_user.bookings.future
     @past_bookings = current_user.bookings.past
