dd6ff3242b22acb21254d63c553c0576ba54557f
Devi Putri <devi.putri@live.com>
2023-08-17 15:07:48 +0900

    updating the maps

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,22 +17,30 @@ export default class extends Controller {
     })
 
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
 
-    #addMarkersToMap() {
-      this.markersValue.forEach((marker) => {
-        // Create a popup
-        const popup = new mapboxgl.Popup().setHTML(marker.popup_html)
-
-        // Create a custom marker
-        const customMarker = document.createElement("div")
-        customMarker.innerHTML = marker.marker_html
-
-        new mapboxgl.Marker(customMarker)
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup)
-          .addTo(this.map)
-      })
-    }
-    
   }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      // Create a popup
+      const popup = new mapboxgl.Popup().setHTML(marker.popup_html)
+
+      // Create a custom marker
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        // .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 500 })
+  }
+
 }
