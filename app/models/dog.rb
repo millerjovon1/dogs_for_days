c00b18d30c4e3e2d27ac42fc6f1982f783cfa3ac
Gabrielle Smer <gabriellesmer@gmail.com>
2023-08-18 16:21:11 +0900

    changesssss

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -13,4 +13,27 @@ class Dog < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  def self.paws_pixels(size)
+    case size
+    when 'small'
+      '24px'
+    when 'medium'
+      '32px'
+    when 'large'
+      '46px'
+    else
+      '24px'
+    end
+  end
+
+  def self.gender_icon(gender)
+    case gender
+    when 'male'
+      '<i class="fa-solid fa-mars" style="color: #3974db;font-size:44px"></i>'.html_safe
+    when 'female'
+      '<i class="fa-solid fa-venus" style="color: #3974db; font-size:44px"></i>'.html_safe
+    else
+      '<i class="fa-solid fa-venus" style="color: #3974db;font-size:44px"></i>'.html_safe
+    end
+  end
 end
